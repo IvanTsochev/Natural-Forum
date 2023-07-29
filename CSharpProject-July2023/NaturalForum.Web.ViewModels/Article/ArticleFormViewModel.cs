274fe789fe119faedd2bd288255410825596d7f5
IvanTsochev <ivantcochev@gmail.com>
2023-07-29 23:10:07 +0300

    Add create article view
    Create view and service for create article

---
+++ CSharpProject-July2023/NaturalForum.Web.ViewModels/Article/ArticleFormViewModel.cs
@@ -0,0 +1,19 @@
+﻿namespace NaturalForum.Web.ViewModels.Article
+{
+	using System.ComponentModel.DataAnnotations;
+	using static Common.EntityValidationConstants.Article;
+	public class ArticleFormViewModel
+    {
+		[Required]
+		[StringLength(TitleMaxLength, MinimumLength = TitleMinLength)]
+		public string Title { get; set; } = null!;
+
+		[Required]
+		[StringLength(DescriptionMaxLength, MinimumLength = DescriptionMinLength)]
+		public string Description { get; set; } = null!;
+
+		[Required]
+		[StringLength(ImageUrlMaxLenght)]
+		public string ImageUrl { get; set; } = null!;
+	}
+}
--- CSharpProject-July2023/NaturalForum.Web/Controllers/ArticleController.cs
+++ CSharpProject-July2023/NaturalForum.Web/Controllers/ArticleController.cs
@@ -1,9 +1,15 @@
 ﻿namespace NaturalForum.Web.Controllers
 {
+    using Microsoft.AspNetCore.Authorization;
     using Microsoft.AspNetCore.Mvc;
+
     using NaturalForum.Services.Data.Interfaces;
     using NaturalForum.Web.ViewModels.Article;
 
+    using static Infrastructure.Extensions.ClaimsPrincipalExtensions;
+    using static Common.NotificationMessagesConstants;
+
+    [Authorize]
     public class ArticleController : Controller
     {
         private readonly IArticleService articleService;
@@ -13,12 +19,45 @@
             this.articleService = articleService;
         }
 
+        [HttpGet]
         public async Task<IActionResult> All()
         {
             IEnumerable<ArticleViewModel> viewModel = 
                 await this.articleService.AllArticlesAsync();
 
             return View(viewModel);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            ArticleFormViewModel viewModel = new ArticleFormViewModel();
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(ArticleFormViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                TempData[ErrorMessage] = "There was an error!";
+
+                return View(model);
+            }
+
+            try
+            {
+                this.articleService.CreateArticleAsync(model, User.GetId());
+
+                TempData[SuccessMessage] = "House was added successfully!";
+                return RedirectToAction("All", "Article");
+            }
+            catch (Exception)
+            {
+                ModelState.AddModelError(string.Empty, "Unexpected error occurred while trying to add your new house! Please try again later or contact administrator!");
+
+                return View(model);
+            }
         }
     }
 }
